a0dc2146c10fd4d9dfdada06091c3d830ba79ad3
Stanley Goldman <Stanley.Goldman@gmail.com>
2018-06-29 12:36:06 -0400

    Add project files.

---
+++ ConsoleCoreAppWithYamlForNoReason/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleCoreAppWithYamlForNoReason
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
